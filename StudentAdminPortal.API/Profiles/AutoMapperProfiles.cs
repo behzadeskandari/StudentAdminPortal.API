e4adc32ebe49f80e9e3135bbdab7d359761860c5
behzad <behzad.b.i.g@gmail.com>
2023-09-26 14:17:25 +0330

    Add project files.

---
+++ StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using StudentAdminPortal.API.DomainModels;
+using StudentAdminPortal.API.Profiles.AfterMaps;
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<DataModel.Student, Student>()
+                .ReverseMap();
+
+            CreateMap<DataModel.Gender, Gender>()
+                .ReverseMap();
+
+            CreateMap<DataModel.Address, Address>()
+                .ReverseMap();
+
+            CreateMap<UpdateStudentRequest, DataModel.Student>()
+                .AfterMap<UpdateStudentRequestAfterMap>();
+
+            CreateMap<AddStudentRequest, DataModel.Student>()
+                .AfterMap<AddStudentRequestAfterMap>();
+        }
+    }
+}
---
+++ StudentAdminPortal.API/Repositories/IStudentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using StudentAdminPortal.API.DataModel;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task<List<Student>> GetStudentsAsync();
+        Task<Student> GetStudentAsync(Guid studentId);
+        Task<List<Gender>> GetGendersAsync();
+        Task<bool> Exists(Guid studentId);
+        Task<Student> UpdateStudent(Guid studentId, Student request);
+        Task<Student> DeleteStudent(Guid studentId);
+        Task<Student> AddStudent(Student request);
+        Task<bool> UpdateProfileImage(Guid studentId, string profileImageUrl);
+    }
+}
